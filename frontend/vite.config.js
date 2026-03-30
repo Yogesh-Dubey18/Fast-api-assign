2b9579f282277acdcc22cff3ff1a26ecd2c09375
Yogesh Dubey <yogidubey07@gmail.com>
2026-03-30 08:46:34 +0530

    Updated backend with CORS for frontend live URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: '.',       // optional, default is '.'
+  base: './',      // important for static hosting on Render
+  build: {
+    outDir: 'dist' // optional, default is 'dist'
+  },
   server: {
     port: 5173,
     open: true,
